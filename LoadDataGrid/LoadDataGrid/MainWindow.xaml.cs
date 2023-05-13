5b178f5f31cd1efcca97e26d5930627f9514855c
SoftwareNuggets <92217545+softwareNuggets@users.noreply.github.com>
2023-05-13 14:48:30 -0400

    Solution for LoadDataGrid with JSON

---
+++ LoadDataGrid/LoadDataGrid/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.IO;
+
+
+namespace LoadDataGrid
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+		
+
+		private void BtnLoad_Click(object sender, RoutedEventArgs e)
+		{
+			string fname = 
+					@"C:\learn\c#\ObservablesExample\LoadDataGrid\LoadDataGrid\members.JSON";
+
+			if (File.Exists(fname) == false)
+			{
+				Console.WriteLine(
+					 $"{fname} is not available");
+				return;
+			}
+
+			var json = 	File.ReadAllText(fname);
+			List<People>? _people = JsonConvert.DeserializeObject<List<People>>(json);
+			
+			//clear out grid
+			GridPeople.ItemsSource = null;
+
+			if (_people != null)
+			{
+				GridPeople.ItemsSource = _people;
+			}
+		}
+
+		private void BtnSave_Click(object sender, RoutedEventArgs e)
+		{
+
+			var data = (List<People>)this.GridPeople.ItemsSource;
+
+			var json =
+				 JsonConvert.SerializeObject(  data,   Formatting.Indented);
+
+
+			string fname =
+				@"C:\learn\c#\ObservablesExample\LoadDataGrid\LoadDataGrid\members.JSON";
+
+			if (File.Exists(fname) == true)
+			{
+				File.Delete(fname);
+			}
+
+			File.WriteAllText(fname, json);
+
+		}
+	}
+}
---
+++ LoadDataGrid/LoadDataGrid/People.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadDataGrid
+{
+
+	public enum JobType : int
+	{
+		Programmer	= 1,
+		DBA			= 2,
+		HelpDesk	= 3
+	};
+	public class People
+	{
+		public string Name { get; set; }
+		public string Email { get; set; }
+		public string Phone { get; set; }
+		public string Address { get; set; }
+		public string City { get; set; }
+		public string State { get; set; }
+		public JobType Job_type { get; set; }
+	}
+}
